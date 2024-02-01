b38683a95aa1962b344ab4f0c1645e9518ebb0cb
toomas5 <115462520+toomas5@users.noreply.github.com>
2024-02-01 14:50:09 +0200

    Initial commit

---
+++ h1.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+<div class="container">
+<h1>yl1</h1>
+<?php
+/*
+yl01
+toomas
+01/02/24
+*/
+
+$nimi = 'toomas';
+$synd = 17;
+$kuju = 'kolmnurk';
+echo "nimi ".$nimi." synniaasta ".$synd." t2htkuju ".$kuju."<br>";
+echo "\"It's My Life\" - Dr. Alban<br>";
+
+echo "(\(\<br>";
+echo "( -.-)<br>";
+echo "o_(\")(\")<br>";
+?>
+
+<h1>y2</h1>
+<?php
+
+$nr1 = 12;
+$nr2 = 5;
+$liit = $nr1 + $nr2;s
+$laht = $nr1 - $nr2;
+$kord = $nr1 * $nr2;
+$jaga = $nr1 / $nr2;
+$j22k = $nr1 % $nr2;
+$v1 = sprintf('12 + 5 = %0.3f <br>', $liit);
+$v2 = sprintf('12 - 5 = %0.3f <br>', $laht);
+$v3 = sprintf('12 * 5 = %0.3f <br>', $kord);
+$v4 = sprintf('12 / 5 = %0.3f <br>', $jaga);
+$v5 = sprintf('12 ¤ 5 = %0.3f <br>', $j22k);
+echo $v1,$v2,$v3,$v4,$v5;
+?>
+</div>
+        <script
+            src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+            integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+            crossorigin="anonymous"
+        ></script>
+
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+            integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
