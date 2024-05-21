c5bb810c97fd91facf7e3f4da9f6433650f4c2f0
toomas5 <115462520+toomas5@users.noreply.github.com>
2024-05-21 08:39:44 +0300

    Create fiveserver.config.js

---
+++ fiveserver.config.js
@@ -0,0 +1,7 @@
+// fiveserver.config.js
+module.exports = {
+    highlight: false, // enable highlight feature
+    injectBody: true, // enable instant update
+    remoteLogs: false, // enable remoteLogs
+    php: "C:\\xampp\\php\\php.exe"
+  };
